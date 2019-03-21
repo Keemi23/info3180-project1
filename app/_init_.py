ca851218b9fc5eb821b854fc32a06622b26c40c7
Akeem Purrier <akeem_purrier@yahoo.com>
2019-03-21 18:16:18 +0000

    final commit

--- app/_init_.py
+++ app/_init_.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "m@@dextreme"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yecsapnzlttgcb:8860d3512549e3ebba04aa68ede74496e30041ff458ea1d46d7c4ed7f5402af0@ec2-54-221-244-196.compute-1.amazonaws.com:5432/d9d6gbbcjoc71g'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://impjobcppvkgbc:5368842d0bd8ebdacf4a628ba3a980c751172c2de7085c23688c8bfe60a37302@ec2-54-83-61-142.compute-1.amazonaws.com:5432/d1r0sbkhohlt2q'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
